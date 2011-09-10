6f3e60195830812bf0f721f6f300632289493685
Alvaro Pereyra <alvaro@xendacentral.com>
2011-09-10 08:17:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetchive::Application.config.secret_token = '707382b3c9fb1732fb7fb576db7464d2d2539d430c71fca48220ec851a66b41b8c318d3218120708a7b63acf5dcce2bc8ac27329b4763893c94708dedc900f0a'
