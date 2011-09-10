2c125dcce6aff9b97b25f5cda314bd0176584dd6
Alvaro Pereyra <alvaro@xendacentral.com>
2011-09-10 09:08:03 -0500

    Updates with Twitter gem support

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem "twitter"
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,16 @@
 class Tweet < ActiveRecord::Base
+
+	before_save :load_from_twitter
+
+	def load_from_twitter
+		tweet_id = tweet_url.split("/").last
+		tweet = Twitter.status(tweet_id)
+		self.screen_name = tweet.user.screen_name
+		self.name = tweet.user.name
+		self.content = tweet.text
+		self.background_url = tweet.user.profile_background_image_url
+		self.tweeted_at = DateTime.parse(tweet.created_at)
+		self.photo_url = tweet.user.profile_image_url
+	end
+
 end
